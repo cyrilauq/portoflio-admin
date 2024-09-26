5e28d3da8b3134384ce425c97ea9bc7ddb7cc0c2
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-26 15:51:36 +0200

    add icon to the html and the necessary imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IconOpenNewWindowComponent } from './component/icons/action/icon-open-new-window/icon-open-new-window.component';
+import { IconGitHubComponent } from './component/icons/social/icon-git-hub/icon-git-hub.component';
+import { IconXComponent } from './component/icons/social/icon-x/icon-x.component';
+import { IconYouTubeComponent } from './component/icons/social/icon-you-tube/icon-you-tube.component';
+import { HeaderComponent } from './component/global/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IconOpenNewWindowComponent, IconGitHubComponent, IconXComponent, IconYouTubeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
