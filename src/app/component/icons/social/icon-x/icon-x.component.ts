df0e5cc05b4a066dbfce924cc7730af752026f94
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-26 15:49:57 +0200

    add x icon

---
+++ src/app/component/icons/social/icon-x/icon-x.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icon-x',
+  standalone: true,
+  imports: [],
+  templateUrl: './icon-x.component.html',
+  styleUrl: './icon-x.component.css'
+})
+export class IconXComponent {
+
+}
