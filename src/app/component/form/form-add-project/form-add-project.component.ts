62ccecc4b5b4525875787baaf6cc0d0d1d957e08
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-26 21:12:50 +0200

    stylize form style and transform it into a stepper

--- src/app/component/form/form-add-project/form-add-project.component.ts
+++ src/app/component/form/form-add-project/form-add-project.component.ts
@@ -1,12 +1,25 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-form-add-project',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './form-add-project.component.html',
   styleUrl: './form-add-project.component.css'
 })
 export class FormAddProjectComponent {
+  step = 0
 
+  onPrevious(event: Event) {
+    event.preventDefault()
+    if(this.step > 0) {
+      this.step--
+    }
+  }
+
+  onNext(event: Event) {
+    event.preventDefault()
+    this.step++
+  }
 }
