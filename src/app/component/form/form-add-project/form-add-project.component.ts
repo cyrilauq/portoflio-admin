b1d3bdcc4f09fbda97d92c3d6577b61e9dca683e
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-27 09:49:40 +0200

    add controls to form

--- src/app/component/form/form-add-project/form-add-project.component.ts
+++ src/app/component/form/form-add-project/form-add-project.component.ts
@@ -1,25 +1,36 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
 
 @Component({
   selector: 'app-form-add-project',
   standalone: true,
-  imports: [NgIf],
+  imports: [NgIf, ReactiveFormsModule],
   templateUrl: './form-add-project.component.html',
   styleUrl: './form-add-project.component.css'
 })
 export class FormAddProjectComponent {
   step = 0
+  public projectForm = new FormGroup({
+    title_control: new FormControl(''),
+    description_control: new FormControl(''),
+    live_link_control: new FormControl(''),
+    technologie_control: new FormControl(''),
+    link_src_control: new FormControl(''),
+    link_title_control: new FormControl('')
+  })
 
-  onPrevious(event: Event) {
+  onPrevious(event: MouseEvent) {
     event.preventDefault()
     if(this.step > 0) {
       this.step--
     }
   }
 
-  onNext(event: Event) {
+  onNext(event: MouseEvent) {
     event.preventDefault()
     this.step++
   }
+
+  onSubmit() {}
 }
