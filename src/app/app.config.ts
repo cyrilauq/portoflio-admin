453357630e8e3c49adf91242b1e2f7bf52ab3fee
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-28 21:04:16 +0200

    inject angular firebase services

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,19 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
+import { environment } from './environments/environment';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    ...AngularFireModule.initializeApp(environment.firebaseConfig).providers!,
+    AngularFirestoreModule
+  ]
 };
