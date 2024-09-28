6ceb76198cb51a7e22e5400aeba17e5ee9824670
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-28 21:05:08 +0200

    add form datas to the firestore

--- src/app/component/form/form-add-project/form-add-project.component.ts
+++ src/app/component/form/form-add-project/form-add-project.component.ts
@@ -1,6 +1,8 @@
 import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
+import ProjectService from '../../../core/services/projectService';
+import Project from '../../../core/models/project';
 
 @Component({
   selector: 'app-form-add-project',
@@ -11,6 +13,8 @@ import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms'
 })
 export class FormAddProjectComponent {
   step = 0
+  miniatureFile: File | undefined
+  screenShotsFiles: Array<File> = []
   public projectForm = new FormGroup({
     title_control: new FormControl(''),
     description_control: new FormControl(''),
@@ -19,6 +23,8 @@ export class FormAddProjectComponent {
     link_src_control: new FormControl(''),
     link_title_control: new FormControl('')
   })
+
+  constructor(private myService: ProjectService) {}
 
   onPrevious(event: MouseEvent) {
     event.preventDefault()
@@ -32,5 +38,35 @@ export class FormAddProjectComponent {
     this.step++
   }
 
-  onSubmit() {}
+  onSubmit() {
+    this.myService.createProject(
+      Project.fromObject({
+        id: '1',
+        name: this.projectForm.get('title_control')?.value || "",
+        description: this.projectForm.get('description_control')?.value || "",
+        links: [{ name: 'front-end', link: 'https://github.com/cyrilauq/cyrilauq.github.io' }],
+        technologies: ['Vue JS 3', 'HTML', 'CSS', 'TypeScript', 'Firebase']
+      }),
+      this.miniatureFile || File.prototype,
+      this.screenShotsFiles)
+    .subscribe({
+      error: alert
+    })
+  }
+
+  onMiniatureSelected(event: Event) {
+    const file: File = (event?.target as any).files[0];
+
+    if (file) {
+      this.miniatureFile = file
+    }
+  }
+
+  onScreenshotsChange(event: Event) {
+    const files: File[] = (event?.target as any).files;
+
+    if (files && files.length > 0) {
+      this.screenShotsFiles.push(...files)
+    }
+  }
 }
