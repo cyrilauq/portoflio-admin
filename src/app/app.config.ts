d8cfd63b12a4c700492cd3089f07efffb6622c51
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-09-30 13:37:38 +0200

    correctly import dependancy as needed for standalone app

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,10 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getFirestore, initializeFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
 import { environment } from './environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
@@ -14,7 +14,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes), 
-    ...AngularFireModule.initializeApp(environment.firebaseConfig).providers!,
-    AngularFirestoreModule
+    importProvidersFrom([AngularFirestoreModule, AngularFireModule.initializeApp(environment.firebaseConfig)])
   ]
 };
