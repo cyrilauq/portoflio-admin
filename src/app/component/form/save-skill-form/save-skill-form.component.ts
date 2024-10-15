090e3a60db3745990f9f8e2c8b7385059288fcf6
Cyril Auquier <cyrilauquier@hotmail.fr>
2024-10-15 19:58:08 +0200

    add a type to a skill

--- src/app/component/form/save-skill-form/save-skill-form.component.ts
+++ src/app/component/form/save-skill-form/save-skill-form.component.ts
@@ -20,6 +20,9 @@ export class SaveSkillFormComponent implements IFormEmitter<SaveSkillFormSubmitA
       Validators.min(1),
       Validators.max(10)
     ]),
+    skill_type: new FormControl('programming_languge', [
+      Validators.required
+    ]),
   });
 
   @Output("formSubmitted") formSubmitted = new EventEmitter<SaveSkillFormSubmitArgs>();
@@ -27,11 +30,13 @@ export class SaveSkillFormComponent implements IFormEmitter<SaveSkillFormSubmitA
   public onSubmit() {
     this.skillForm.get('skill_name_control')?.markAsDirty()
     this.skillForm.get('skill_expertise_control')?.markAsDirty()
+    this.skillForm.get('skill_type')?.markAsDirty()
     this.skillForm.updateValueAndValidity()
     if(this.skillForm.valid) {
       this.formSubmitted.emit({
         skillName: this.skillForm.get('skill_name_control')?.value || "",
-        skillExperise: this.skillForm.get('skill_expertise_control')?.value || 1
+        skillExperise: this.skillForm.get('skill_expertise_control')?.value || 1,
+        skillType: this.skillForm.get('skill_type')?.value || "programming_languge"
       })
     } else {
       alert("One or more fields aren't valid")
@@ -41,5 +46,6 @@ export class SaveSkillFormComponent implements IFormEmitter<SaveSkillFormSubmitA
 
 export class SaveSkillFormSubmitArgs {
   public skillName!: string;
+  public skillType!: string;
   public skillExperise!: number
 }
--- src/app/core/models/skill.ts
+++ src/app/core/models/skill.ts
@@ -1,13 +1,15 @@
 export default class Skill {
     name: string
     expertise: number
+    type: string
 
-    constructor(object: { name: string, expertise: number }) {
+    constructor(object: { name: string, expertise: number, type: string }) {
         this.name = object.name
         this.expertise = object.expertise
+        this.type = object.type
     }
 
-    static fromObject(object: { name: string, expertise: number }) {
+    static fromObject(object: { name: string, expertise: number, type: string }) {
         return new Skill(object)
     }
 }
